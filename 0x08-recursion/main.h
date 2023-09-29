52f9e9a933426e9ca238308fb751b2cb92031a41
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-09-29 23:17:26 +0300

    fix error in main.h&*.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #ifndef MAIN_H
 #define MAIN_H
-int _puthchar(char c);
+int _putchar(char c);
 void _puts_recursion(char* s);
 void _print_rev_recursion(char* s);
 int _strlen_recursion(char* s);
