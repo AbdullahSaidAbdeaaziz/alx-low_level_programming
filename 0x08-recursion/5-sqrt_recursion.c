9de8a9734de2890e9ab31e7aae945d5978cd4051
AbdullahSaidAbdeaaziz <githubjoe2@gmail.com>
2023-09-30 19:14:44 +0300

    Fix in order of function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * sqrt_helper - Helper function to calculate the square root recursively
+ * @n: The number for which to calculate the square root
+ * @x: The current guess for the square root
+ *
+ * Return: The integer square root of n
+ */
+int sqrt_helper(int n, int x)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+
+	if (x * x > n)
+	{
+		return (-1);
+	}
+
+	return (sqrt_helper(n, x + 1));
+}
+
+/**
  * _sqrt_recursion - Computes the square root of a number using recursion
  * @n: The number for which to calculate the square root
  *
@@ -21,24 +43,3 @@ return (n);
 return (sqrt_helper(n, 1));
 }
 
-/**
- * sqrt_helper - Helper function to calculate the square root recursively
- * @n: The number for which to calculate the square root
- * @x: The current guess for the square root
- *
- * Return: The integer square root of n
- */
-int sqrt_helper(int n, int x)
-{
-if (x * x == n)
-{
-return (x);
-}
-
-if (x * x > n)
-{
-return (-1);
-}
-
-return (sqrt_helper(n, x + 1));
-}
