b3c93ff5f17d01ac4f495aefd1ee38c2dca71986
AbdullahSaidAbdeaaziz <githubjoe2@gmail.com>
2023-09-30 19:12:42 +0300

    Task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Computes the square root of a number using recursion
+ * @n: The number for which to calculate the square root
+ *
+ * Return: The integer square root of n, or -1
+ * if n does not have an integer square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0 || n == 1)
+{
+return (n);
+}
+
+return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - Helper function to calculate the square root recursively
+ * @n: The number for which to calculate the square root
+ * @x: The current guess for the square root
+ *
+ * Return: The integer square root of n
+ */
+int sqrt_helper(int n, int x)
+{
+if (x * x == n)
+{
+return (x);
+}
+
+if (x * x > n)
+{
+return (-1);
+}
+
+return (sqrt_helper(n, x + 1));
+}
