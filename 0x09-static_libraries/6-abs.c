49a50c195bbe72119288a322d4e3f89c66a56f3b
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-10-02 22:06:44 +0300

    Adding statics files task1

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+
+int _abs(int n)
+{
+    return (n < 0 ? n * -1: n);
+}
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+2-strlen.c
+2-strncpy.c
+3-islower.c
+3-puts.o
+3-strcmp.c
+3-strspn.c
+4-isalpha.c
+4-strpbrk.c
+5-strstr.c
+6-abs.c
+9-strcpy.c
+*/
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+#endif
