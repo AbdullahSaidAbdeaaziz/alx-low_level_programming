3604b705d6482051821e5fe3fe0c6075acdcedfb
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-10-03 01:00:57 +0300

    FirstTask

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - say my name
+ * @argc: length of options
+ * @argv: list of options
+ * pass to program
+ *
+ * Return: 1 success
+*/
+
+int main(int argc, char **argv)
+{
+(void) argc;
+printf("%s", argv[0]);
+return (1);
+}
