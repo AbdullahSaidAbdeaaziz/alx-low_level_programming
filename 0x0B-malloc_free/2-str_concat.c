1fe3dc9a1111b7843f569f426babbfba7754d6c3
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-10-04 22:43:11 +0300

    thirdTask

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * str_concat - combine 2 string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: combined string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+char *all_str;
+size_t total_size, i, size1, size2;
+if (s1 == NULL && s2 == NULL)
+{
+return (NULL);
+}
+size1 = strlen(s1);
+size2 = strlen(s2);
+total_size = size1 + size2;
+all_str = (char *)malloc(total_size *sizeof(char) + 1);
+
+if (all_str == NULL)
+{
+return (NULL);
+}
+
+i = 0;
+while (i < total_size)
+{
+if (i < size1)
+{
+all_str[i] = s1[i];
+}
+else
+{
+all_str[i] = s2[i - size1];
+}
+i++;
+}
+return (all_str);
+}
