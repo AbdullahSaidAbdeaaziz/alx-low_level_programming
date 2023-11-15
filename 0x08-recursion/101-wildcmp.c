96ab171fcd7c9378a9557964b300d9c187cb55ce
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-15 15:37:10 +0200

    Fix final task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,9 +15,11 @@ return (1);
 if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
+{
 if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1)
 return (1);
 else
 return (wildcmp(s1, s2 + 1));
+}
 return (0);
 }
