6cd33e296d2fee61aaa4e1a52cbff4470812c23d
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-17 10:27:24 +0200

    Implement no.of options pass to *.exe file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print length of options
+ * @argc: length of options
+ * @argv: list of options
+ *
+ *
+ * Return: 1 success
+ */
+
+int main(int argc, char *argv[])
+{
+int i, no_options = 0;
+(void)argc;
+for (i = 1; i < argc; i++)
+{
+if (argv[i] != NULL)
+{
+no_options += 1;
+}
+}
+printf("%d", no_options);
+return (1);
+}
