c4bd4b1f2177184fd76ef24d85300c166f12c66d
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-18 03:54:22 +0200

    try if will success or not

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print list of options
+ * @argc: length of options
+ * @argv: list of options
+ *
+ *
+ * Return: 1 success
+ */
+
+int main(int argc, char *argv[])
+{
+    int i;
+    (void)argc;
+    i = 0;
+    while(argv[i] != NULL)
+    {
+    printf("%d\n", void());
+    }
+    return (1);
+}
