6f54483e0ab5c20906cf0e925f66459773fa0974
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-19 15:20:31 +0200

    task 3 mult 2 operand

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiple 2 number pass by options
+ * @argc: length of options
+ * @argv: list of options
+ *
+ *
+ * Return: 1 success
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc - 1 != 2) {
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (1);
+}
