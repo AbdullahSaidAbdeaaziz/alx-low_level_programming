0ce88e53981002f69fa0185bdbfb12b27c8851ad
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-24 21:21:32 +0200

    check solution

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: number we will allocate in memory
+ *
+ * Return: 98 if malloc is failed, otherwise pointer
+ * to allocate memory.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *allocate;
+allocate = malloc(sizeof(b));
+if (allocate == NULL)
+{
+exit(98);
+}
+return (allocate);
+}
