2dca812320341f03ff829c9ef44d1fea0d76349b
AbdullahSaidAbdeaaziz <githubjoe2@gmail.com>
2023-11-25 08:02:35 +0200

    fix betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,35 +13,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *str;
-    size_t i, j, s1_len, s2_len;
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
-    if (n < s2_len){
-        s2_len = n;
-    }
-    str = malloc(s1_len + s2_len + 1);
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    for(i = 0; i < s1_len; i++)
-    {
-        str[i] = s1[i];
-    }
-    for(j = 0; j < s2_len; j++, i++)
-    {
-        str[i] = s2[j];
-    }
-    str[i] = '\0';
-    return (str);
+char *str;
+size_t i, j, s1_len, s2_len;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+s1_len = strlen(s1);
+s2_len = strlen(s2);
+if (n < s2_len)
+{
+s2_len = n;
+}
+str = malloc(s1_len + s2_len + 1);
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < s1_len; i++)
+{
+str[i] = s1[i];
+}
+for (j = 0; j < s2_len; j++, i++)
+{
+str[i] = s2[j];
+}
+str[i] = '\0';
+return (str);
 }
 
