ca34664ea197c2b37d20e540d6722aaf65392a3d
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-28 01:53:10 +0200

    misunderstanding of requirement so I fix it

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,25 +9,8 @@
 
 void print_dog(struct dog *d)
 {
-if (!d)
+if (d)
 {
-printf("nil\n");
-return;
-}
-if (!d->name)
-{
-printf("Name: nil\n");
-}
-if (!d->owner)
-{
-printf("Owner: nil\n");
-}
-if (d->age <= 0)
-{
-printf("Age: nil\n");
-}
-if (d->owner && d->age && d->age > 0)
-{
-printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s", !d->name ? "nil": d->name, d->age, !d->owner ? "nil": d->owner);
 }
 }
