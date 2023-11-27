3236a04ea9a97755afadde24bd4340afae51b1c7
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-28 01:27:30 +0200

    adding init_dog for initialize struct dog variables

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - dog datatype
  * @name: name of dog
@@ -14,5 +15,7 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
