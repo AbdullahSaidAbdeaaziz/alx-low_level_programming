9872d21502380054347438829e41b6ac3405827a
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-29 23:05:58 +0200

    second task action on array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,12 @@
+#include <stddef.h>
 #ifndef FUNC_POINTERS
 #define FUNC_POINTERS
 
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 #endif
