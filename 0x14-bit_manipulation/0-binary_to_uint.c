d466ae6b40cee484f8f2e4ffe10546c4fc910f3d
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-13 19:44:19 +0200

    fix[0-binary_to_uint.c]: output of code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,4 @@
 #include "main.h"
-#include <string.h>
-
-/**
- * _pow - get power of base number
- * @base: base
- * @exp: exponent
- * Return: power of number
- */
-size_t _pow(int base, int exp)
-{
-	if (exp == 0)
-	{
-		return (1);
-	}
-	return (base * _pow(base, exp - 1));
-}
-
-/**
- * check_binary - check if string is binary or not
- * @n: string of binary number
- * Return: 1 if is binary, otherwise 0
- */
-unsigned int check_binary(const char *n)
-{
-	size_t i = 0;
-
-	while (n[i] != '\0')
-	{
-		if (n[i] != '1' && n[i] != '0')
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
 
 /**
  * binary_to_uint - convert binary to decimal
@@ -42,25 +6,26 @@ unsigned int check_binary(const char *n)
  * Return: decimal if correct representation,
  * otherwise 0
  */
+
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i = strlen(b) - 1, exp = 0;
-	unsigned int result = 0;
+	unsigned int num = 0;
 
-	if (!b || !check_binary(b))
-	{
+	if (!b)
 		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		num <<= 1;
+		if (*b == '1')
+			num |= 1;
+
+		b++;
 	}
 
-
-	while (i >= 0)
-	{
-		result += (b[i] - '0') * _pow(2, exp++);
-		i--;
-	}
-	if (result == 0)
-	{
-		return (1);
-	}
-	return (result);
+	return (num);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 
