0b342e455acf66d1d21a036efc3e1e07595a4897
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-13 20:31:16 +0200

    Naming[4-clear_bit.c]: function name wrong

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * set_bit - set bit to 0 at
+ * clear_bit - set bit to 0 at
  * specific index in number
  * @n: decimal
  * @index: index of bit in decimal
  * Return: number with setted bit on it,
  * otherwise -1
-*/
+ */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index);
 {
 	if (index > (sizeof(*n) * 8 - 1))
 		return (-1);
