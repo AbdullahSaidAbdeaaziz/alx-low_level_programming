f6782fda2faca21abbe67191e168f62961ccf021
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-13 20:40:24 +0200

    Adding[5-flip_bits.c]: no.of bits need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flipt_bits - the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: number
+ * @m: no.of flips
+ * Return: number of bits needs to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_val = n ^ m;
+	unsigned int count = 0;
+
+	while (xor_val)
+	{
+		count += xor_val & 1;
+		xor_val >>= 1;
+	}
+
+	return (count);
+}
