f2aa9446363d6206f742416b3f91c66952521e23
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-13 20:42:33 +0200

    Format[5-flip_bits.c]: fix misspell in description

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * flipt_bits - the number of bits you would need to
- * flip to get from one number to another.
+ * flip_bits - the number of bits you would need to
+ * flip to get from one number to another
  * @n: number
  * @m: no.of flips
  * Return: number of bits needs to flip
